2d282379ef23d2b71f4e5f45b197d04dd974e116
senaye_mekdes <107151653+senayemekdes@users.noreply.github.com>
2024-05-27 12:37:00 -0700

    Fix Hero Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
 import Hero from './Components/Hero'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <Hero />
-<<<<<<< HEAD
-  
-=======
->>>>>>> 65f4736767b0aa501f31078bd3e21f20fdf2b3d6
-
     </>
   )
 }
